6b49cb688a03e7414b61b35aa7a07e2f1e9fa103
awerbrouck <arthur.jasaw@gmail.com>
2020-11-27 16:48:36 +0100

    adding tables and updating file

--- PostgreSQL_code.sql
+++ PostgreSQL_code.sql
@@ -1,66 +1,103 @@
-CREATE TABLE ToeristischeActiviteit(
-Regio varchar NOT NULL,
-TypeActiviteit varchar NOT NULL,
-O_ID integer NOT NULL,
-PrijsPerPersoon integer NOT NULL,
-Straat varchar,
-Postcode integer,
-Naam varchar
-Gemeente varchar NOT NULL,
-Huisnummer integer,
-Beschrijving varchar,
-Website varchar,
-Tel varchar,
-T_ID integer NOT NULL,
-PRIMARY KEY (Postcode, Naam)
+CREATE TABLE ToeristischeActiviteit
+(
+	toeristische_regio        varchar NOT NULL,
+	activiteittype            varchar NOT NULL,
+	openingstijd_ID           integer NOT NULL,
+	PrijsPerPersoon           integer NOT NULL,
+	straat                    varchar,
+	postcode                  integer,
+	activiteitnaam            varchar,
+	gemeente                  VARCHAR NOT NULL,
+	huisnummer                integer,
+	beschrijving              varchar,
+	activiteit_website        varchar,
+	activiteit_telefoonnummer varchar,
+	toegankelijkheids_ID      integer NOT NULL,
+	PRIMARY KEY (postcode, activiteitnaam)
 );
 
-CREATE TABLE Openingstijd(
-O_ID integer,
-Eindtijd time,
-Starttijd time,
-Datum date,
-Postcode integer,
-Naam varchar,
-PRIMARY KEY (O_ID),
-CONSTRAINT fk_ToeristischeActiviteit
-FOREIGN KEY(Postcode, Naam)
-REFERENCES ToeristischeActiviteit(Postcode, Naam)
+CREATE TABLE inschrijving
+(
+	emailpersoon varchar,
+	postcode     varchar,
+	naam         varchar,
+	aantal       integer,
+	bevestigd    boolean,
+	tijdstip     timestamp,
+	PRIMARY KEY (emailpersoon, postcode, naam)
+);
+
+CREATE TABLE boekingen
+(
+	emailpersoon varchar,
+	hotel_id     varchar,
+	tijdstip     timestamp,
+	begin        timestamp,
+	eind         timestimp,
+	aantal       integer,
+	PRIMARY KEY (emailpersoon, hotel_id)
+);
+
+CREATE TABLE kortingen
+(
+	activiteitnaam      varchar,
+	activiteit_postcode varchar,
+	hotelid             varchar,
+	percentage_korting  varchar,
+	PRIMARY KEY (activiteitnaam, activiteit_postcode, hotelid)
+
+);
+
+CREATE TABLE Openingstijd
+(
+	O_ID      integer,
+	Eindtijd  time,
+	Starttijd time,
+	Datum     date,
+	Postcode  integer,
+	Naam      varchar,
+	PRIMARY KEY (O_ID),
+	CONSTRAINT fk_ToeristischeActiviteit
+		FOREIGN KEY (Postcode, Naam)
+			REFERENCES ToeristischeActiviteit (Postcode, Naam)
 );
 
 ALTER TABLE Openingstijd
-ADD CONSTRAINT validTijdCheck
-CHECK (Eindtijd > Starttijd);
+	ADD CONSTRAINT validTijdCheck
+		CHECK (Eindtijd > Starttijd);
 
-CREATE TABLE Toegankelijkheidsinfo(
-T_ID integer NOT NULL,
-Info varchar NOT NULL,
-Naam varchar NOT NULL,
-Postcode integer NOT NULL,
-PRIMARY KEY(T_ID),
-CONSTRAINT fk_ToeristischeActiviteit
-FOREIGN KEY(Postcode, Naam)
-REFERENCES ToeristischeActiviteit(Postcode, Naam)
+CREATE TABLE Toegankelijkheidsinfo
+(
+	toegankelijkheids_ID integer NOT NULL,
+	Info                 varchar NOT NULL,
+	Naam                 varchar NOT NULL,
+	Postcode             integer NOT NULL,
+	PRIMARY KEY (toegankelijkheids_ID),
+	CONSTRAINT fk_ToeristischeActiviteit,
+	FOREIGN KEY (Postcode, Naam)
+		REFERENCES ToeristischeActiviteit (Postcode, Naam)
 );
 
-CREATE TABLE Persoon(
-Email varchar,
-Voornaam varchar NOT NULL,
-Achternaam varchar NOT NULL,
-PRIMARY KEY(Email)
+CREATE TABLE Persoon
+(
+	Email      varchar,
+	Voornaam   varchar NOT NULL,
+	Achternaam varchar NOT NULL,
+	PRIMARY KEY (Email)
 );
 
-CREATE TABLE Hotel(
-Beschrijving varchar,
-Sterren varchar,
-MinPrijs integer,
-Email varchar,
-H_ID integer,
-Naam varchar NOT NULL,
-Regio varchar NOT NULL,
-Gemeente varchar NOT NULL,
-Postcode integer NOT NULL,
-Huisnummer integer NOT NULL,
-Straat varchar NOT NULL,
-PRIMARY KEY(H_ID)
+CREATE TABLE Hotel
+(
+	Beschrijving varchar,
+	Sterren      varchar,
+	MinPrijs     integer,
+	Email        varchar,
+	H_ID         integer,
+	Naam         varchar NOT NULL,
+	Regio        varchar NOT NULL,
+	Gemeente     varchar NOT NULL,
+	Postcode     integer NOT NULL,
+	Huisnummer   integer NOT NULL,
+	Straat       varchar NOT NULL,
+	PRIMARY KEY (H_ID)
 );
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ TODO
 ---------------------------------------------
 - [X] Relaties toevoegen in het eerd 
 - [X] Dubbele lijnen toevoegen in het eerd
-- [ ] Functionele beschrijving in eerd
-- [ ] Logisch ontwerp maken -> Aanvulling van de relaties, de andere informatie is reeds weergegeven in de functionele beschrijving.
+- [X] Functionele beschrijving in eerd
+- [X] Logisch ontwerp maken -> Aanvulling van de relaties, de andere informatie is reeds weergegeven in de functionele beschrijving.
