61fe1a549e818d9752ee391c1bf597ded271cce4
awerbrouck <arthur.jasaw@gmail.com>
2020-12-11 16:02:10 +0100

    yeet

--- TFV/overlapping.sql
+++ TFV/overlapping.sql
@@ -4,14 +4,14 @@ $BODY$
 DECLARE
 BEGIN
 
-	IF EXISTS(SELECT 1 FROM openingstijd r WHERE
-		(r.begintijdstip <= NEW.begintijdstip) AND
-		(NEW.begintijdstip < r.begintijdstip + r.duur) AND
-		r.restonaam = NEW.restonaam) THEN
+	IF EXISTS(SELECT 1 FROM openingstijd a WHERE
+		(a.starttijd <= NEW.starttijd) AND
+		(NEW.begintijdstip < a.begintijdstip + a.duur) AND
+		a.activiteit = NEW.activiteit) THEN
 		RAISE EXCEPTION 'opening time of resto %, begin period % and end period % overlaps with already existing opening time.', NEW.restonaam, NEW.begintijdstip, NEW.begintijdstip + NEW.duur;
 	END IF;
 
-	IF EXISTS(SELECT 1 FROM openingstijd r WHERE
+	IF EXISTS(SELECT 1 FROM openingstijd a WHERE
 		(r.begintijdstip < NEW.begintijdstip + NEW.duur) AND
 		(NEW.begintijdstip + NEW.duur <= r.begintijdstip + r.duur) AND
 		r.restonaam = NEW.restonaam) THEN
