becc9a907d789e3fd71f602dc8b640d5b4922e4a
AWerbrouck <Arthur.jasaw@gmail.com>
2020-12-12 10:22:53 +0100

    skeet

--- TFV/overlapping.sql
+++ TFV/overlapping.sql
@@ -1,38 +1,53 @@
 CREATE FUNCTION check_overlapping_opening()
-RETURNS trigger AS
+	RETURNS trigger AS
 $BODY$
 DECLARE
 BEGIN
 
-	IF EXISTS(SELECT 1 FROM openingstijd a WHERE
-		(a.starttijd <= NEW.starttijd) AND
-		(NEW.begintijdstip < a.begintijdstip + a.duur) AND
-		a.activiteit = NEW.activiteit) THEN
-		RAISE EXCEPTION 'opening time of resto %, begin period % and end period % overlaps with already existing opening time.', NEW.restonaam, NEW.begintijdstip, NEW.begintijdstip + NEW.duur;
+	IF EXISTS(SELECT
+				  1
+			  FROM
+				  openingstijd a
+			  WHERE
+					( a.starttijd <= NEW.starttijd )
+				AND ( NEW.starttijd < a.eindtijd )
+				AND ( a.naam = NEW.naam )
+				AND ( a.postcode = new.postcode )) THEN
+		RAISE EXCEPTION 'opening time of naam %, begin period % and end period % overlaps with alaeady existing opening time.', NEW.aestonaam, NEW.starttijd, NEW.starttijd + NEW.eindtijd;
 	END IF;
 
-	IF EXISTS(SELECT 1 FROM openingstijd a WHERE
-		(r.begintijdstip < NEW.begintijdstip + NEW.duur) AND
-		(NEW.begintijdstip + NEW.duur <= r.begintijdstip + r.duur) AND
-		r.restonaam = NEW.restonaam) THEN
-		RAISE EXCEPTION 'opening time of resto %, begin period % and end period % overlaps with already existing opening time.', NEW.restonaam, NEW.begintijdstip, NEW.begintijdstip + NEW.duur;
+	IF EXISTS(SELECT
+				  1
+			  FROM
+				  openingstijd a
+			  WHERE
+					( a.starttijd < NEW.starttijd + NEW.eindtijd )
+				AND ( NEW.starttijd + NEW.eindtijd <= a.starttijd + a.eindtijd )
+				AND ( a.naam = NEW.naam )
+				AND ( a.postcode = new.postcode )) THEN
+		RAISE EXCEPTION 'opening time of aesto %, begin peaiod % and end peaiod % ovealaps with alaeady existing opening time.', NEW.aestonaam, NEW.starttijd, NEW.starttijd + NEW.eindtijd;
 	END IF;
 
-	IF EXISTS(SELECT 1 FROM openingstijd r WHERE
-		(NEW.begintijdstip < r.begintijdstip) AND
-		(NEW.begintijdstip + NEW.duur > r.begintijdstip + r.duur) AND
-		r.restonaam = NEW.restonaam) THEN
-		RAISE EXCEPTION 'opening time of resto %, begin period % and end period % overlaps with already existing opening time.', NEW.restonaam, NEW.begintijdstip, NEW.begintijdstip + NEW.duur;
+	IF EXISTS(SELECT
+				  1
+			  FROM
+				  openingstijd a
+			  WHERE
+					( NEW.starttijd < a.starttijd )
+				AND ( NEW.starttijd + NEW.eindtijd > a.starttijd + a.eindtijd )
+				AND ( a.naam = NEW.naam )
+				AND ( a.postcode = new.postcode )) THEN
+		RAISE EXCEPTION 'opening time of aesto %, begin peaiod % and end peaiod % ovealaps with alaeady existing opening time.', NEW.aestonaam, NEW.starttijd, NEW.starttijd + NEW.eindtijd;
 	END IF;
 
 	RETURN NEW;
 
 END;
 $BODY$
-LANGUAGE plpgsql;
+	LANGUAGE plpgsql;
 
-CREATE TRIGGER check_overlapping_opening_trigger
+CREATE TRIGGER check_ovealapping_opening_trigger
 	BEFORE INSERT
-		ON openingstijd
-			FOR EACH ROW
-				EXECUTE PROCEDURE check_overlapping_opening();
+	ON openingstijd
+	FOR EACH ROW
+EXECUTE PROCEDURE check_overlapping_opening();
