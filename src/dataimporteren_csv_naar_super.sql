017bf6c291a4c896a60826da75f3db03de6846b4
MaximStockmans <contact@maximstockmans.com>
2020-12-13 10:29:42 +0100

    add data importeren csv to super relations

---
+++ src/dataimporteren_csv_naar_super.sql
@@ -0,0 +1,16 @@
+--change path relative to local csv files
+
+COPY super_activiteitreservaties(activiteittype, activiteitnaam, straat, huisnummer, postcode, gemeente, toeristische_regio, activiteit_telefoonnummer, activiteit_website, beschrijving, activiteit_toegang_doof, activiteit_toegang_slechthorend, activiteit_toegang_mentaal, activiteit_toegang_motorisch, activiteit_toegang_blind, activiteit_toegang_slechtziend, activiteit_toegang_autisme, activiteit_prijs, begintijdstip, duur, voornaam, achternaam, persoon_email, boeking_bevestigd, aantal_personen, persoon_doof, persoon_slechthorend, persoon_mentaal, persoon_motorisch, persoon_blind, persoon_slechtziend, persoon_autisme)
+FROM '/home/maxim/Documents/Informatica/Bachelor_1/Semester_1/Databases/Project/DatabankenProject/data/activiteitreservaties.csv'
+DELIMITER ';'
+CSV HEADER;
+
+COPY super_hotelboekingen(hotelid, hotelnaam, straat, huisnummer, postcode, gemeente, toeristische_regio, hotel_email, hotel_beschrijving, aantal_sterren, minimumprijs, voornaam, achternaam, persoon_email, boekingstijdstip, boeking_bevestigd, begindatum, einddatum, aantal_personen)
+FROM '/home/maxim/Documents/Informatica/Bachelor_1/Semester_1/Databases/Project/DatabankenProject/data/hotelboekingen.csv'
+DELIMITER ';'
+CSV HEADER;
+
+COPY super_kortingen(activiteitnaam, activiteit_postcode, hotelid, percentage_korting)
+FROM '/home/maxim/Documents/Informatica/Bachelor_1/Semester_1/Databases/Project/DatabankenProject/data/kortingen.csv'
+DELIMITER ';'
+CSV HEADER; 
