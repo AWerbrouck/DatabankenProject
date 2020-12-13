0d52c53eeba88550a7983e13f1e2a8a276cdf711
AWerbrouck <Arthur.jasaw@gmail.com>
2020-12-13 10:01:37 +0100

    skeet

--- fysiekOntwerp.sql
+++ fysiekOntwerp.sql
@@ -1,24 +1,24 @@
 CREATE TABLE ToeristischeActiviteit
 (
-	toeristische_regio        varchar NOT NULL,
-	activiteittype            varchar NOT NULL,
-	openingstijd_ID           integer NOT NULL,
-	PrijsPerPersoon           integer NOT NULL,
-	straat                    varchar,
-	postcode                  varchar,
-	activiteitnaam            varchar,
-	gemeente                  VARCHAR NOT NULL,
-	huisnummer                varchar,
-	beschrijving              varchar,
-	activiteit_website        varchar,
-	activiteit_telefoonnummer varchar,
+	toeristische_regio              varchar NOT NULL,
+	activiteittype                  varchar NOT NULL,
+	openingstijd_ID                 integer NOT NULL,
+	PrijsPerPersoon                 integer NOT NULL,
+	straat                          varchar,
+	postcode                        varchar,
+	activiteitnaam                  varchar,
+	gemeente                        VARCHAR NOT NULL,
+	huisnummer                      varchar,
+	beschrijving                    varchar,
+	activiteit_website              varchar,
+	activiteit_telefoonnummer       varchar,
 	activiteit_toegang_doof         varchar,
 	activiteit_toegang_slechthorend varchar,
-        activiteit_toegang_mentaal      varchar,
-        activiteit_toegang_motorisch    varchar,
-        activiteit_toegang_blind        varchar,
-        activiteit_toegang_slechtziend  varchar,
-        activiteit_toegang_autisme      varchar,
+	activiteit_toegang_mentaal      varchar,
+	activiteit_toegang_motorisch    varchar,
+	activiteit_toegang_blind        varchar,
+	activiteit_toegang_slechtziend  varchar,
+	activiteit_toegang_autisme      varchar,
 	PRIMARY KEY (postcode, activiteitnaam)
 );
 
@@ -41,26 +41,26 @@ CREATE TABLE Hotel
 	Regio        varchar NOT NULL,
 	Gemeente     varchar NOT NULL,
 	Postcode     varchar NOT NULL,
-	Huisnummer   varchar  NOT NULL,
+	Huisnummer   varchar NOT NULL,
 	Straat       varchar NOT NULL,
 	PRIMARY KEY (H_ID)
 );
 
 CREATE TABLE inschrijving
 (
-	emailpersoon varchar,
-	postcode     varchar,
-	naam         varchar,
-	aantal       integer,
-	bevestigd    boolean,
-	tijdstip     timestamp,
-	persoon_doof                    varchar,
-    	persoon_slechthorend            varchar,
-    	persoon_mentaal                 varchar,
-    	persoon_motorisch                varchar,
-    	persoon_blind                   varchar,
-    	persoon_slechtziend             varchar,
-    	persoon_autisme                 varchar,
+	emailpersoon         varchar,
+	postcode             varchar,
+	naam                 varchar,
+	aantal               integer,
+	bevestigd            boolean,
+	tijdstip             timestamp,
+	persoon_doof         varchar,
+	persoon_slechthorend varchar,
+	persoon_mentaal      varchar,
+	persoon_motorisch    varchar,
+	persoon_blind        varchar,
+	persoon_slechtziend  varchar,
+	persoon_autisme      varchar,
 	PRIMARY KEY (emailpersoon, postcode, naam),
 	CONSTRAINT fk_toeristischeactiviteit
 		FOREIGN KEY (postcode, naam)
@@ -78,10 +78,10 @@ CREATE TABLE boekingen
 	emailpersoon varchar,
 	hotel_id     varchar,
 	tijdstip     timestamp,
-	begintijd        timestamp,
-	eindtijd         timestamp,
+	begintijd    timestamp,
+	eindtijd     timestamp,
 	aantal       integer,
-	bevestigd boolean,
+	bevestigd    boolean,
 	PRIMARY KEY (emailpersoon, hotel_id),
 	CONSTRAINT fk_hotel
 		FOREIGN KEY (hotel_id)
@@ -95,7 +95,7 @@ CREATE TABLE kortingen
 (
 	activiteitnaam      varchar,
 	activiteit_postcode varchar,
-	hotel_id             varchar,
+	hotel_id            varchar,
 	percentage_korting  varchar,
 	PRIMARY KEY (activiteitnaam, activiteit_postcode, hotel_id),
 	CONSTRAINT fk_hotel
@@ -109,12 +109,12 @@ CREATE TABLE kortingen
 
 CREATE TABLE Openingstijd
 (
-	openingstijd_ID      varchar,
-	Eindtijd  timestamp,
-	Starttijd timestamp,
-	Datum     date,
-	Postcode  varchar,
-	Naam      varchar,
+	openingstijd_ID varchar,
+	Eindtijd        timestamp,
+	Starttijd       timestamp,
+	Datum           date,
+	Postcode        varchar,
+	Naam            varchar,
 	PRIMARY KEY (openingstijd_ID),
 	CONSTRAINT fk_ToeristischeActiviteit
 		FOREIGN KEY (Postcode, Naam)
