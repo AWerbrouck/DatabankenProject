830dbff1675544cec71b8b6587d4904d944922a1
MaximStockmans <contact@maximstockmans.com>
2020-12-13 14:06:14 +0100

    change query 1

--- src/db_queries_groep3.sql
+++ src/db_queries_groep3.sql
@@ -1,22 +1,23 @@
 --QUERY 1
 --Tel totale aantal unieke boekingen die werden geregistreerd, zowel op hotels als toeristische activiteit
 SELECT
-COUNT(sub1.email) + COUNT(sub2.email)
+COUNT(sub1.email)
 FROM
 (	SELECT DISTINCT
-	b.emailpersoon AS email,
-	b.hotel_ID,
-	b.begintijd
+	b.emailpersoon::varchar AS email,
+	b.hotel_ID::varchar,
+	b.begintijd::varchar,
+	1::varchar
 	FROM
-	boeking b	
-) sub1,
-(	SELECT DISTINCT
-	i.emailpersoon AS email,
-	i.postcode,
-	i.naam,
-	i.tijdstip
+	boekingen b	
+    UNION
+	SELECT DISTINCT
+	i.emailpersoon::varchar AS email,
+	i.postcode::varchar,
+	i.naam::varchar,
+	i.tijdstip::varchar
 	FROM
 	inschrijving i
-) sub2;
+) sub1;
 
 --QUERY 2
